8671b9ae62cf3f4c4fb7f138c0af92fa2fad726b
Philipp Minges <fjnalta@gmail.com>
2017-07-06 21:42:49 +0200

    Initial commit

---
+++ js/loadRSS.js
@@ -0,0 +1,37 @@
+function createEntry(link, title, date, id) {
+    "use strict";
+    var myFeed = document.createElement("P");
+    var mylink = document.createElement("A");
+    mylink.setAttribute('href', link);
+    mylink.appendChild(document.createTextNode(title));
+    myFeed.appendChild(document.createTextNode(date));
+    myFeed.appendChild(document.createTextNode(" - "));
+    myFeed.appendChild(mylink);
+    document.getElementById(id).appendChild(myFeed);
+}
+
+function loadRSS(feed) {
+    "use strict";
+    var twitchFeed;
+    
+    switch (feed) {
+    case "ezlife":
+        twitchFeed = "https://ezlife.eu/apps/gitlab/philippm/ezlife.eu/commits/master.atom";
+        break;
+    default:
+        break;
+    }
+    
+    $.ajax(twitchFeed, {
+        accepts: {
+            xml: "application/xml"
+        },
+        dataType: "xml",
+        success: function (data) {
+            $(data).find("entry").slice(0, 10).each(function () {
+                var el = $(this);
+                createEntry(el.find("id").text(), el.find("title").text(), el.find("updated").text(), feed);
+            });
+        }
+    });
+}
