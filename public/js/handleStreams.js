a4b2ba728a593ef7ae5f880266d919f2f4f6a915
philipp <fjnalta@gmail.com>
2020-01-04 01:16:07 +0100

    handle twitch streams, rework stream ui

---
+++ public/js/handleStreams.js
@@ -0,0 +1,38 @@
+function streamButtonClicked(stream) {
+    loadStream(stream);
+    loadStreamChat(stream);
+
+    scrollTo("#currentStream");
+}
+
+function scrollTo(element){
+    $('html,body').animate({scrollTop: ($(element).offset().top)},'slow');
+}
+
+
+function loadStream(stream) {
+    $("#current-stream").empty();
+    $("#current-stream-heading").empty();
+    $("#current-stream-heading").append("<h2><img src=\"img/logos/stream/twitch.png\" id=\"currentStream\" style=\"max-height: 50px\"> Stream - " + stream + "</h2>");
+    $("#current-stream").append("<div class=\"embed-responsive embed-responsive-16by9\" id=\"twitch-player\"></div>\n" +
+        "<script type=\"text/javascript\">\n" +
+        "var options = {\n" +
+        "height: 300,\n" +
+        "channel: '" + stream + "'};" +
+        "var player = new Twitch.Player(\"twitch-player\", options);\n" +
+        "player.setVolume(0.5);\n" +
+        "</script>");
+}
+
+function loadStreamChat(stream) {
+    $("#current-stream-chat").empty();
+    $("#current-stream-heading").empty();
+    $("#current-stream-heading").append("<h2><img src=\"img/logos/stream/twitch.png\" id=\"currentStream\" style=\"max-height: 50px\"> Stream - " + stream + "</h2>");
+    $("#current-stream-chat").append("<iframe frameborder=\"0\"\n" +
+        "scrolling=\"no\"\n" +
+        "id=\"chat_embed\"\n" +
+        "src=\"https://www.twitch.tv/embed/" + stream + "/chat\"\n" +
+        "height=\"500\"\n" +
+        "width=\"100%\">\n" +
+        "</iframe>");
+}
