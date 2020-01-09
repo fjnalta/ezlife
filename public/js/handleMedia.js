0e043a6b594085705334ce4544d85d1eb39de4ce
philipp <fjnalta@gmail.com>
2020-01-09 16:45:10 +0100

    implement media, rework index

--- public/js/handleMedia.js
+++ public/js/handleMedia.js
@@ -4,3 +4,25 @@ $(document).ready(function () {
         $("#tabContent").attr("hidden",false);
     });
 });
+
+function videoButtonClicked(videoId) {
+    loadVideo(videoId);
+    scrollTo("#currentVideo");
+}
+
+function scrollTo(element){
+    $('html,body').animate({scrollTop: ($(element).offset().top)},'slow');
+}
+
+
+function loadVideo(videoId) {
+    $("#current-video").empty();
+    $("#current-video-heading").empty();
+    $("#current-video-heading").append("<h2><img src=\"img/logos/stream/youtube.png\" id=\"currentVideo\" style=\"max-height: 50px\">Current Video</h2>");
+    $("#current-video").append('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://youtube.com/embed/' + videoId + '" allowfullscreen></iframe></div>');
+}
+
+function removeVideo() {
+    $("#current-video-heading").empty();
+    $("#current-video").empty();
+}
