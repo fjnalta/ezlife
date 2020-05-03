a13666bb1d6d379962b48a4fb59b7de73bd7ab12
ajo <fjnalta@gmail.com>
2020-05-03 19:41:22 +0200

    implement twitch clips

--- public/js/handleMedia.js
+++ public/js/handleMedia.js
@@ -10,6 +10,11 @@ function videoButtonClicked(videoId) {
     scrollTo("#currentVideo");
 }
 
+function clipButtonClicked(clip) {
+    loadClip(clip);
+    scrollTo("#currentVideo");
+}
+
 function scrollTo(element){
     $('html,body').animate({scrollTop: ($(element).offset().top)},'slow');
 }
@@ -20,6 +25,13 @@ function loadVideo(videoId) {
     $("#current-video-heading").empty();
     $("#current-video-heading").append("<h2><img src=\"img/logos/stream/youtube.png\" id=\"currentVideo\" style=\"max-height: 50px\">Current Video</h2>");
     $("#current-video").append('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="https://youtube.com/embed/' + videoId + '" allowfullscreen></iframe></div>');
+}
+
+function loadClip(clip) {
+    $("#current-video").empty();
+    $("#current-video-heading").empty();
+    $("#current-video-heading").append("<h2><img src=\"img/logos/stream/twitch.png\" id=\"currentVideo\" style=\"max-height: 50px\">Current Video</h2>");
+    $("#current-video").append('<div class="embed-responsive embed-responsive-16by9"><iframe class="embed-responsive-item" src="' + clip + '" allowfullscreen></iframe></div>');
 }
 
 function removeVideo() {
