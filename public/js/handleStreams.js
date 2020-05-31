cf73c403f5854d940af67638754d8b7be0a8e608
ajo <fjnalta@gmail.com>
2020-05-31 18:11:56 +0200

    rework stream loading

--- public/js/handleStreams.js
+++ public/js/handleStreams.js
@@ -1,10 +1,3 @@
-$(document).ready(function () {
-    $("#tabContent").fadeIn("slow", function () {
-        $("#loadingSpinner").attr("hidden",true);
-        $("#tabContent").attr("hidden",false);
-    });
-});
-
 function streamButtonClicked(stream) {
     loadStream(stream);
     loadStreamChat(stream);
