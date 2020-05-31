ef0c856398c71667c7e3b80250c9c1d69eb1259c
ajo <fjnalta@gmail.com>
2020-05-31 18:04:59 +0200

    rework development page - use r2 for external requests, fix loading spinner

--- public/js/handleDevelopment.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function () {
-    $("#tabContent").fadeIn("slow", function () {
-        $("#loadingSpinner").attr("hidden",true);
-        $("#tabContent").attr("hidden",false);
-    });
-});
