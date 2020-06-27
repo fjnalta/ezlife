5cff1399171113785868a5b37fbbb20a2076f4fc
ajo <ajo@ezlife.eu>
2020-06-27 21:27:34 +0200

    EZLIFE-34 prepare code cleanup

---
+++ public/js/handleSettings.js
@@ -0,0 +1,8 @@
+function changePassword() {
+
+    console.log($('#currentPasswordInput').val());
+    console.log($('#newPasswordInput').val());
+    console.log($('#repeatPasswordInput').val());
+
+    // TODO - handle google recaptcha on settings page globally
+}
